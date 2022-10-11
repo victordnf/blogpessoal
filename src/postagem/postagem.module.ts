cd00a08784cb88deddbc52d7faded6bfa6694d72
Victor França <vito.daleste@gmail.com>
2022-10-11 16:00:39 -0300

    Mensagem

---
+++ src/postagem/postagem.module.ts
@@ -0,0 +1,17 @@
+import { Controller, Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { TemaService } from "src/tema/service/tema.service";
+import { TemaModule } from "src/tema/tema.module";
+import { PostagemController } from "./controllers/postagem.controller";
+import { Postagem } from "./entities/postagem.entity";
+import { PostagemService } from "./services/postagem.service";
+
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Postagem]), TemaModule],
+    providers: [PostagemService, TemaService],
+    controllers: [PostagemController],
+    exports: [TypeOrmModule]
+})
+
+export class PostagemModule {}
